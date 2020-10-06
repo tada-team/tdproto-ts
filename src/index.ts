000fa95ecd90f43a280517ae57fd82f46b85b8bb
mv-go <mv.gololobov@gmail.com>
2020-10-06 17:57:19 +0300

    feat(types): provide basic type comments

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
+/**
+ * Links to an icon with size parameters.
+ */
 export interface Icon {
+    /**
+     * Icon height in pixels.
+     */
     height: number;
+
+    /**
+     * Icon width in pixels.
+     */
     width: number;
+
+    /**
+     * Full URL string to icon image.
+     */
     url: string;
 }
 
+/**
+ * A set of different sized Icon objects with a fallback.
+ */
 export interface Icons {
+    /**
+     * Small sized Icon.
+     */
     sm: Icon;
+
+    /**
+     * Large sized Icon.
+     */
     lg: Icon;
+
+    /**
+     * Fallback stub.
+     */
     stub: string;
 }
